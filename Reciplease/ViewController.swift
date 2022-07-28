b08ed034d4c1665d4f2bab518d79aab4dafeba54
Yann Rouzaud <yann.rouzaud@gmail.com>
2022-07-28 14:36:50 +0200

    Initial Commit

---
+++ Reciplease/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reciplease
+//
+//  Created by Yann Rouzaud on 28/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
